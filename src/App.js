5e7e08e4ca11b088b690a94dc66ad6c02b0f2bef
Gaz Ellis <gmiellis@yahoo.co.uk>
2019-03-13 16:32:06 +0000

    replaced number results with odometer and added some overall fomatting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Odometer from "react-odometerjs";
+
+import "odometer/themes/odometer-theme-slot-machine.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      arr: [0,0,0,0,0,0]
+      arr: [0,0,0,0,0,0],
+      odometerValue: 0
+
     };
     this.handleGenerateNumbers = this.handleGenerateNumbers.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -43,24 +48,38 @@ class App extends Component {
   }
   
   render() {
+    const { odometerValue } = this.state;
     return (
 
       <div className="App">
+
         <h1>Lottery Numbers<br/>Generator</h1>
         <h3>Press<br/>the<br/>button</h3>
+
         <button onClick={this.handleGenerateNumbers}>Generate Numbers</button>
         <button onClick={this.handleReset}>Reset</button>
         
         <div>
-          <p>
-            {
+          {/* <p> */}
+            {/* {
               this.state.arr.map((number, index) => {
                 return <span key={index}> {number} </span>
               })
+            } */}
+            {
+              this.state.arr.map((number, index) => {
+                return <span>
+                         <Odometer
+                            key={index}
+                            format="dd" 
+                            duration={1000} 
+                            value={number} />
+                      </span> 
+              })
             }
-          </p>
+          {/* </p> */}
         </div>
-        
+       
       </div>
     );
   }
